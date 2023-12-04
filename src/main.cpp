700d5cbf6d74708c37ef2e51c54d6a8d5e50ca8f
Shyed Shahriar <shyed2001@yahoo.com>
2023-12-04 10:13:43 +0600

    Update main.cpp
    //-Use** const** and **reference * *modifiers for your getter methods.This will prevent unnecessary copying and modification of your class members.For example, instead of `std: :string getDescription()`, you can write `const std::string& getDescription() const`.
    //- Use * *smart pointers * *instead of raw pointers.This will help you avoid memory leaks and simplify memory management.For example, instead of `TodoItem* newItem = new TodoItem(); `, you can write `std::unique_ptr<TodoItem> newItem = std::make_unique<TodoItem>(); `.
    //- Use * *std::vector * *instead of std::list.This will improve your performance and reduce memory fragmentation.For example, instead of `std::list<TodoItem> todoItems; `, you can write `std::vector<TodoItem> todoItems; `.
    //- Use * *std::find_if * *instead of manual iteration.This will make your code more readable and expressive.For example, instead of `for (it = todoItems.begin(); it != todoItems.end(); it++) {
    // if (input_id == it->getId()) { it->setCompleted(true); break; } }`,
    // you can write `auto it = std::find_if(todoItems.begin(), todoItems.end(), [input_id](const TodoItem& item)
    // { return input_id == item.getId(); }); if (it != todoItems.end())
    // { it->setCompleted(true); }`.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,9 @@
-/** cpp-todo-list
-* Small console based application in C++ to demonstrate a basic
-* Todo application
-* @erikyuzwa
-* License: MIT
-*/
-
 #include <iostream>
 #include <string>
-#include <list>
+#include <vector>
 #include <ctime>
+#include <memory>
+#include <algorithm>
 
 class TodoItem {
 private:
@@ -17,7 +12,10 @@ private:
     bool completed;
 
 public:
+
     TodoItem() : id(0), description(""), completed(false) {}
+
+
     ~TodoItem() = default;
 
     bool create(std::string new_description) {
@@ -27,9 +25,12 @@ public:
         return true;
     }
 
-    int getId() { return id; }
-    std::string getDescription() { return description; }
-    bool isCompleted() { return completed; }
+    int getId() const { return id; }
+
+    // std::string getDescription() { return description; }
+
+    const std::string& getDescription() const { return description; }
+    bool isCompleted() const { return completed; }
 
     void setCompleted(bool val) { completed = val; }
 
@@ -41,10 +42,13 @@ int main()
     int input_id;
     std::string input_description;
     std::string version = "v0.2.0";
-    std::list<TodoItem> todoItems;
-    std::list<TodoItem>::iterator it;
+    //std::list<TodoItem> todoItems;
+    std::vector<std::unique_ptr<TodoItem>> todoItems;
 
-    srand(time(NULL));
+    // srand(time(NULL));
+    // Avoid warning 
+    srand(static_cast<unsigned int>(time(NULL)));
+
 
     todoItems.clear();
 
@@ -53,11 +57,11 @@ int main()
         std::cout << "Todo List Maker - " << version << std::endl;
         std::cout << std::endl << std::endl;
 
-        for (it = todoItems.begin(); it != todoItems.end(); it++) {
+        for (const auto& item : todoItems) {
 
-            std::string completed = it->isCompleted() ? "done" : "not done";
+            std::string completed = item->isCompleted() ? "done" : "not done";
 
-            std::cout << it->getId() << " | " << it->getDescription() << " | "
+            std::cout << item->getId() << " | " << item->getDescription() << " | "
                 << completed << std::endl;
         }
 
@@ -85,29 +89,43 @@ int main()
             std::cin.ignore();
             std::getline(std::cin, input_description);
 
-            TodoItem newItem;
-            newItem.create(input_description);
-            todoItems.push_back(newItem);
+            //TodoItem newItem;
+            //newItem.create(input_description);
+            //todoItems.push_back(newItem);
+
+            std::unique_ptr<TodoItem> newItem = std::make_unique<TodoItem>();
+            newItem->create(input_description);
+            todoItems.push_back(std::move(newItem));
 
         }
         else if (input_option == 'c') {
             std::cout << "Enter id to mark completed: ";
             std::cin >> input_id;
 
-            for (it = todoItems.begin(); it != todoItems.end(); it++) {
+            //for (it = todoItems.begin(); it != todoItems.end(); it++) {
 
-                if (input_id == it->getId()) {
-                    it->setCompleted(true);
-                    break;
-                }
+            //    if (input_id == it->getId()) {
+            //        it->setCompleted(true);
+            //        break;
+            //    }
+            //}
+
+            auto it = std::find_if(todoItems.begin(), todoItems.end(), [input_id](const auto& item)
+                {
+                    return input_id == item->getId();
+                });
+
+            if (it != todoItems.end()) {
+                (*it)->setCompleted(true);
             }
-
         }
+
 
     }
 
     return 0;
 }
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
@@ -119,3 +137,14 @@ int main()
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+
+//-Use** const** and **reference * *modifiers for your getter methods.This will prevent unnecessary copying and modification of your class members.For example, instead of `std: :string getDescription()`, you can write `const std::string& getDescription() const`.
+//- Use * *smart pointers * *instead of raw pointers.This will help you avoid memory leaks and simplify memory management.For example, instead of `TodoItem* newItem = new TodoItem(); `, you can write `std::unique_ptr<TodoItem> newItem = std::make_unique<TodoItem>(); `.
+//- Use * *std::vector * *instead of std::list.This will improve your performance and reduce memory fragmentation.For example, instead of `std::list<TodoItem> todoItems; `, you can write `std::vector<TodoItem> todoItems; `.
+//- Use * *std::find_if * *instead of manual iteration.This will make your code more readable and expressive.For example, instead of `for (it = todoItems.begin(); it != todoItems.end(); it++) { 
+// if (input_id == it->getId()) { it->setCompleted(true); break; } }`,
+// you can write `auto it = std::find_if(todoItems.begin(), todoItems.end(), [input_id](const TodoItem& item)  
+// { return input_id == item.getId(); }); if (it != todoItems.end()) 
+// { it->setCompleted(true); }`.
+
